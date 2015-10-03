b6f416101d8a39b8a04af26b27ea285c2f2b82fa
swapnil3667 <aswap1989@gmail.com>
2015-10-03 16:08:48 +0800

    Some more addition

--- readData.py
+++ readData.py
@@ -6,11 +6,18 @@ def Generate_KNN(userid,videoid):
 	with open('/home/swapnil/Documents/KDD/20150701094451-Rakuten-Viki-Data/20150701094451-Behavior_training.csv', 'rb') as csvfile:
 		lines = csv.reader(csvfile)
 		dataset = list(lines)
+		userlist = []
 		for x in range(len(dataset)-1):
 			if(dataset[x][1] == userid):
 				print dataset[x]
 			if(dataset[x][2] == videoid):
-                        	print dataset[x]
+				print dataset[x]
+				userlist.append(dataset[x][1])
+
+		for x in range(len(dataset)-1):
+			user = dataset[x][1]
+			if user in userlist:
+				print dataset[x]
 
 
 if __name__ == "__main__":
@@ -19,4 +26,3 @@ if __name__ == "__main__":
 	userid = content[0]
 	videoid = content[1]
 	Generate_KNN(userid[:len(userid)-1],videoid[:len(videoid)-1])
-

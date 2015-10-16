4197d48b22e85c05ba1489189a956f26ba595dcb
swapnil3667 <aswap1989@gmail.com>
2015-10-17 02:25:53 +0800

    View Creation is Fine

--- readDataWithSql.py
+++ readDataWithSql.py
@@ -25,13 +25,49 @@ try:
       # Now print fetched result
       print "date=%s,user_id=%s,videoid=%s,mv_ratio=%s,rating=%s" % \
              (fname, lname, age, sex, income )
+
+   sql = "CREATE VIEW V AS SELECT behavior_training.user_id, behavior_training.video_id, \
+           user_attributes.country, user_attributes.gender, score, origin_country, genres, \
+           person_id, video_casts.country as cast_country , video_casts.gender as cast_gender \
+           FROM (((behavior_training INNER JOIN user_attributes) \
+           INNER JOIN  video_attributes) INNER JOIN video_casts)"
+
+   cursor.execute(sql)
+
+   sql = "SELECT * FROM V \
+           LIMIT 1 "
+
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      user_id = row[0]
+      video_id = row[1]
+      country = row[2]
+      gender = row[3]
+      score = row[4]
+      origin_country = row[5]
+      genre = row[6]
+      person_id = row[7]
+      cast_country = row[8]
+      cast_gender = row[9]
+
+      # Now print fetched result
+      print "user_id=%s,videoid=%s,country=%s,gender=%s,score=%s,origin_country=%s,genre=%s,person_id=%s,cast_country=%s,cast_gender=%s" % \
+             (user_id, video_id, country, gender, score, origin_country, genre, person_id, cast_country, cast_gender )
+
+   sql = "DROP VIEW V"
+   cursor.execute(sql)
+
 except Exception as ex:
     template = "An exception of type {0} occured. Arguments:\n{1!r}"
     message = template.format(type(ex).__name__, ex.args)
     print message
+
 #except:
  #  print "Error: unable to fecth data"
 
 
 # disconnect from server
+
 db.close()

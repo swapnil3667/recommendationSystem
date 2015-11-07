538f5c06d46d15087bba11ec2b6270c4c7c5b539
sheenanasim <sheenanasim@r-3-103-25-172.comp.nus.edu.sg>
2015-11-07 15:29:31 +0800

    committing svd code

---
+++ SVD/SVDProcessing.py
@@ -0,0 +1,51 @@
+#import scipy.sparse.linalg.svds as svd
+import numpy as np
+from scipy.sparse.linalg import eigen, svds as sparse_svd
+import pprint
+
+# For printing the complete array values.
+np.set_printoptions(threshold=np.inf)
+
+#array1 = np.genfromtxt('/Users/sheenanasim/Documents/CS5228/project/knn.csv',delimiter = ",")
+#print type(array1)
+#print array1[1]
+import pickle
+with open('/Users/sheenanasim/Documents/CS5228/project/result_user_9-3.out', 'r') as f:
+    new_data = pickle.load(f)
+    
+svdInputMatrix = np.array(new_data,dtype=np.float)
+#print array[1]
+print type(svdInputMatrix)
+print svdInputMatrix.shape
+u,s,vt = sparse_svd(svdInputMatrix)
+print u.shape, s.shape, vt.shape
+print s
+
+energy = 0
+for i in range(len(s)):
+    energy = energy + (s[i]*s[i])
+energy = (energy * 90)/100
+print energy
+
+V = np.transpose(vt)
+print V.shape
+print type(V)
+query = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 2, 3, 1, 2, 2, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+query1 = np.array(query)
+
+result = np.dot(query1,V)
+print result
+
+fresult = np.dot(result,vt)
+index = fresult.argsort()[-10:]
+ 
+print index
+
+print fresult[fresult.argsort()[-10:]]
+print fresult[21]
+print fresult[184]
+
+
+
+
+
